a59761dbedb79c4e4f9c0fd4f2a3765662b59fc5
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-22 15:33:02 +0530

    [Mahesh]Add: add data in reverse order

--- LinkedListProblems/LinkedList.cs
+++ LinkedListProblems/LinkedList.cs
@@ -1,35 +1,63 @@
-﻿using System;
+﻿using LinkedListProblems;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
-namespace LinkedListProblems
+namespace LinkedListProblem
 {
-        public class LinkedList
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
         {
-            public Node head;
-            public void Add(int data)
+            Node node = new Node(data);
+            if (this.head == null)
             {
-                Node node = new Node(data);
-                if (this.head == null)
-                {
-                    this.head = node;
-                }
-                else
-                {
-                    Node temp = head;
-                    while (temp.next != null)
-                    {
-                        temp = temp.next;
-                    }
-                    temp.next = node;
-                }
-                Console.WriteLine("{0} inserted into linked list", node.data);
+                this.head = node;
             }
-
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
         }
-    }
 
-    
+        public void AddInReverseOrder(int data) //70,30,56
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-namespace LinkedListProblems
+namespace LinkedListProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Operations");
-            LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Linked List Operations");
+                Console.WriteLine(" 1. Add LinkedList\n 2. Add data in reverse order");
+                Console.WriteLine("Enter above mention option");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        LinkedList list = new LinkedList();
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+
+                    case 2:
+                        LinkedList linkedList = new LinkedList();
+                        linkedList.AddInReverseOrder(70);
+                        linkedList.AddInReverseOrder(30);
+                        linkedList.AddInReverseOrder(56);
+                        linkedList.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+
+            }
         }
     }
 }
