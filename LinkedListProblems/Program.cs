00dd5210572b6154eb78d3b3bf79f6a8fc27d703
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-22 15:23:49 +0530

    [Mahesh]Add: Create linked list

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
