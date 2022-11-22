63c3c284d2aad9e67cb49e23ac4756c61446e934
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-22 15:38:24 +0530

    [Mahesh]Add: Insert the data at particular position

--- LinkedListProblems/Program.cs
+++ LinkedListProblems/Program.cs
@@ -5,31 +5,36 @@ namespace LinkedListProblem
     {
         static void Main(string[] args)
         {
+            LinkedList list = new LinkedList();
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to Linked List Operations");
-                Console.WriteLine(" 1. Add LinkedList\n 2. Add data in reverse order");
+                Console.WriteLine(" 1. Add LinkedList\n 2. Add data in reverse order\n 3. Insert data at particular position ");
                 Console.WriteLine("Enter above mention option");
                 int option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
                 {
                     case 1:
-                        LinkedList list = new LinkedList();
                         list.Add(56);
                         list.Add(30);
                         list.Add(70);
                         break;
 
                     case 2:
-                        LinkedList linkedList = new LinkedList();
-                        linkedList.AddInReverseOrder(70);
-                        linkedList.AddInReverseOrder(30);
-                        linkedList.AddInReverseOrder(56);
-                        linkedList.Display();
+                        list.AddInReverseOrder(70);
+                        list.AddInReverseOrder(30);
+                        list.AddInReverseOrder(56);
+                        list.Display();
                         break;
                     case 3:
+                        list.Add(56);
+                        list.Add(70);
+                        list.InsertAtParticularPosition(1, 30);
+                        list.Display();
+                        break;
+                    case 4:
                         flag = false;
                         Console.WriteLine("Try Again");
                         break;
